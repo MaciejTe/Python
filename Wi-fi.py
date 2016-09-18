bff6197b645dd890504db0e4c7a370f0edcaf65a
LukasPolon <lukaspolon@gmail.com>
2016-09-18 20:57:34 +0200

    Update

--- Wi-fi.py
+++ Wi-fi.py
@@ -1,133 +1,122 @@
-
-import time, threading
-from iperf_threads import threads_TCP_Download,threads_TCP_Upload
-from iperf_threads import threads_UDP_Download,threads_UDP_Upload, CPE_configuration
+import time
+from iperf_threads import *
 from read_config import read_config
 from Error_handler import Error_handler
 
-class main():
+class main(object):
     def __init__(self):
         read = read_config()
         read.start()
-        self.__list_hostname = read.get_list_hostname()
-        self.__list_username = read.get_list_username()
-        self.__list_port = read.get_list_port()
-        self.__list_password = read.get_list_password()
-        self.__list_master_ip = read.get_list_master_ip()
-        self.__list_CPE_credentials = read.get_list_CPE_credentials()
-        self.__list_channels = read.get_list_channels()
+        #można użyć stałego słownika, ale trzeba doczytać jak :P
+        self.__conf_data = {
+            'hostname': read.get_list_hostname(),
+            'username': read.get_list_username(),
+            'port': read.get_list_port(),
+            'password': read.get_list_password(),
+            'master_ip': read.get_list_master_ip(),
+            'CPE_credentials': read.get_list_CPE_credentials(),
+            'channels': read.get_list_channels()
+        }
+
         self.__err_handler = Error_handler()
 
-'''
-#Struktura
-    def metoda(self,param, enum):
+    def __runThread(self, thr_desc, channel,
+                    wait_switch=True, host_index=0, filename='log0'):
+
+        threads = {'CPE_conf': CPE_configuration(
+                                    channel,
+                                    host_index,
+                                    'CPE_configuration',
+                                    self.__conf_data),
+                   'TCP_upload': threads_TCP_Upload(
+                                    filename,
+                                    host_index,
+                                    "CH%s_TCP_U_%s" % (
+                                        str(channel),
+                                        str(self.__conf_data
+                                            ['hostname'][host_index])),
+                                    self.__conf_data),
+                   'TCP_download': threads_TCP_Download(
+                                    filename,
+                                    host_index,
+                                    "CH%s_TCP_D_%s" % (
+                                        str(channel),
+                                        str(self.__conf_data
+                                            ['hostname'][host_index])),
+                                    self.__conf_data),
+                   'UDP_upload': threads_UDP_Upload(
+                                    filename,
+                                    host_index,
+                                    "CH%s_UDP_U_%s" % (
+                                        str(channel),
+                                        str(self.__conf_data
+                                            ['hostname'][host_index])),
+                                    self.__conf_data),
+                   'UDP_download': threads_UDP_Download(
+                                    filename,
+                                    host_index,
+                                    "CH%s_UDP_D_%s" % (
+                                        str(channel),
+                                        str(self.__conf_data
+                                            ['hostname'][host_index])),
+                                    self.__conf_data)
+                   }
+
         try:
-
-            definicja watku
-
+            thread = threads[thr_desc]
+            thread.start()
+            time.sleep(2)
+            #Dodać obsługę błędów!!!
         except:
-            exc e
+            pass
         finally:
-            try:
-                watek start()
-                time.sleep(2)
-                while
-                sprawdzanie rezultatu    (zmienna static z klasy error_handler)
-'''
+            self.__waitForThread(wait_switch)
+    
+    def __waitForThread(self, switch=True):
+        if switch:
+            while len(threading.enumerate()) is not 1:
+                pass
 
-
-    def one_host(self):
+    def __writeDescription(self, description, hosts_amount='ONE_HOST'):
         try:
-            file_output = open('output.txt', 'a')
-            file_output_full = open('output_full.txt','a')
-            file_output_full.write('********** START - ONE HOST *******************\n')
-            file_output.write('***START - ONE HOST*******************\n')
-            file_output.close()
-            file_output_full.close()
-
-            for channel in self.__list_channels:
-                thread_CPE = CPE_configuration(channel, 'CPE_configuration', self.__list_CPE_credentials[0], self.__list_CPE_credentials[1], self.__list_CPE_credentials[2])
-                thread_CPE.start()
-                time.sleep(2)
-                while (len(threading.enumerate()) !=1):
-                    pass
-
-
-#### TODO #####
-# 1. Skonsolidowac ponizsze funkcje w jedna, ktora dodatkowo zawiera cos w stylu enuma bedacego ID danej operacji
-# 2. Zastosowac rekurencje w celu powtorzenia danego fragmentu kodu
-# 3. Stworzyc metode do while'a
-# 4. uzupelnic w reszcie klas wyjatki - pod koniec
-# 5. Analogicznie wszystkie punkty dla wielu hostow
-
-############## DO POCZYTANIA ####################3
-# 1. Zmienne i metody static
-# 2. Zmienne klasowe, Dostep i uzycie zmiennych klasowych
-# 3. Python telnet - opcjonalnie
-
-######## DO DOKONCZENIA, TU GDZIES BLAD jest
-                if thread_CPE.get_func_state() != 0:
-                    result = self.__err_handler.err_search(thread_CPE.get_func_state())
-                    if(result == 0):
-                        raise Exception('Result - 0, tu bedzie powtorka kodu')
-                    else:
-                        raise Exception('Brejk de program')
-
-                thread2 = threads_TCP_Upload('log0', "CH%s_TCP_U_%s" % (str(channel), str(self.__list_hostname[0])),
-                                             self.__list_hostname[0], self.__list_port[0], self.__list_username[0],
-                                             self.__list_password[0], self.__list_master_ip[0])
-                thread2.start()
-                time.sleep(2)
-                while (len(threading.enumerate()) !=1):
-                    pass
-
-                if thread2.get_func_state() == -1 or thread2.get_func_state() == -2:
-                    raise Exception('Cos nie tak z konfiguracja')
-
-                thread1 = threads_TCP_Download('log0', "CH%s_TCP_D_%s" % (str(channel), str(self.__list_hostname[0])),
-                                               self.__list_hostname[0], self.__list_port[0], self.__list_username[0],
-                                               self.__list_password[0], self.__list_master_ip[0])
-                thread1.start()
-                time.sleep(2)
-                while (len(threading.enumerate()) != 1):
-                    pass
-
-                if thread1.get_func_state() == -1 or thread1.get_func_state() == -2:
-                    raise Exception('Cos nie tak z konfiguracja')
-
-                thread4 = threads_UDP_Upload('log0', "CH%s_UDP_U_%s" % (str(channel), str(self.__list_hostname[0])),
-                                             self.__list_hostname[0], self.__list_port[0], self.__list_username[0],
-                                             self.__list_password[0], self.__list_master_ip[0])
-                thread4.start()
-                time.sleep(2)
-                while (len(threading.enumerate()) != 1):
-                    pass
-
-                if thread4.get_func_state() == -1 or thread4.get_func_state() == -2:
-                    raise Exception('Cos nie tak z konfiguracja')
-
-                thread3 = threads_UDP_Download('log0', "CH%s_UDP_D_%s" % (str(channel), str(self.__list_hostname[0])),
-                                               self.__list_hostname[0], self.__list_port[0], self.__list_username[0],
-                                               self.__list_password[0], self.__list_master_ip[0])
-                thread3.start()
-                time.sleep(2)
-                while (len(threading.enumerate()) != 1):
-                    pass
-
-                if thread3.get_func_state() == -1 or thread3.get_func_state() == -2:
-                    raise Exception('Cos nie tak z konfiguracja')
+            blank_lines = ('\n', '\n\n\n')
+            select_blanklns = 0
+            if hosts_amount != 'ONE_HOST':
+                select_blanklns = 1
 
             file_output = open('output.txt', 'a')
             file_output_full = open('output_full.txt', 'a')
-            file_output.write('***END - ONE HOST*********************\n\n\n')
-            file_output_full.write('***END - ONE HOST*********************\n\n\n')
+            file_output_full.write('********** %s - %s *******************%s'
+                                   %(description, hosts_amount,
+                                     blank_lines[select_blanklns]))
+            file_output.write('*** %s - %s *******************%s'
+                              % (description, hosts_amount,
+                                 blank_lines[select_blanklns]))
             file_output.close()
             file_output_full.close()
+        except:
+            #Dokończyć obsługę błędów!
+            pass
 
-        except IOError as IO:
-            print(IO)
-        except Exception as e:
-            print(e)
+    def one_host(self):
+        try:
+            self.__writeDescription('START')
+            
+            for channel in self.__conf_data['channels']:
+
+                self.__runThread('CPE_conf', channel)
+                self.__runThread('TCP_upload', channel)
+                self.__runThread('TCP_download', channel)
+                self.__runThread('UDP_upload', channel)
+                self.__runThread('UDP_download', channel)
+
+            self.__writeDescription('END')
+
+
+        except:
+            #Obsługa błędów!!!!
+            pass
+
 
 
 
--- iperf_exec.py
+++ iperf_exec.py
@@ -1,53 +1,55 @@
-import os, time, re
+import os
+import time
+
 from SSH_connection import SSH_connection
 
-class iperf(SSH_connection):
-    def __init__(self, filename, hostname, username, password, port_iperf, master_IP):
-        SSH_connection.__init__(self,hostname, username,password)
-        self.__port_SSH = 22
-        self.__port_iperf = port_iperf
-        self.__master_IP = master_IP
-        self.__hostname = hostname
-        self.__username = username
-        self.__password = password
-        self.__s = None
+class iperf(object, SSH_connection):
+    def __init__(self,filename, conf_data, host_index):
+        SSH_connection.__init__(self, conf_data, host_index)
+        self.__port_iperf = conf_data['port'][host_index]
+        self.__master_IP = conf_data['master_ip'][host_index]
+        self.__hostname = conf_data['hostname'][host_index]
+        self.__username = conf_data['username'][host_index]
+        self.__password = conf_data['password'][host_index]
         self.__filename = filename
+        self.__port_SSH = 22
+        self.__s = None
 
     def TCP_Download(self):
-
-        #print(SSH_connection.get_paramiko_object(self))
-        #self.list_parameters()
         self.__s = SSH_connection.connect_to_host(self)
-
-        if(type(self.__s) == type(1)):
+        if self.__s is int:
             return self.__s
         else:
-            stdin, stdout, stderr = self.__s.exec_command('iperf -s -i 1 -p %s' % str(self.__port_iperf))
+            self.__s.exec_command('iperf -s -i 1 -p %s'
+                                  % str(self.__port_iperf))
             time.sleep(3)
-            os.system('iperf -c %s -i 1 -p %s |tee %s' % (self.__hostname, str(self.__port_iperf), self.__filename))
+            os.system('iperf -c %s -i 1 -p %s |tee %s'
+                      % (self.__hostname,
+                         str(self.__port_iperf),
+                         self.__filename))
             time.sleep(2)
             print ("****************************************************")
-            stdin, stdout, stderr = self.__s.exec_command('killall iperf')
+            self.__s.exec_command('killall iperf')
             self.__s.close()
-            #stdin, stdout, stderr = self.s.exec_command('ps ax|grep iperf')
             return 0
 
     def TCP_Upload(self):
         self.__s = SSH_connection.connect_to_host(self)
 
-        if (type(self.__s) == type(1)):
+        if self.__s is int:
             return self.__s
         else:
-            os.system('iperf -s -i 1 -p %s | tee %s &' % (str(self.__port_iperf), self.__filename))
+            os.system('iperf -s -i 1 -p %s | tee %s &'
+                      % (str(self.__port_iperf), self.__filename))
             time.sleep(2)
-            stdin, stdout, stderr = self.__s.exec_command('iperf -c %s -i 1 -p %s' % (self.__master_IP, str(self.__port_iperf)))
+            stdout = self.__s.exec_command('iperf -c %s -i 1 -p %s'
+                                           % (self.__master_IP,
+                                              str(self.__port_iperf)))
             print(stdout.read())
             time.sleep(2)
             print ("****************************************************")
             os.system('killall iperf')
             self.__s.close()
-            #stdin, stdout, stderr = self.s.exec_command('ps ax|grep iperf')
-            #os.system('ps ax|grep iperf')
             return 0
 
     def UDP_Download(self):
@@ -58,14 +60,17 @@ class iperf(SSH_connection):
         elif (self.__s == -2):
             return -2
         else:
-            stdin, stdout, stderr = self.__s.exec_command('iperf -s -i 1 -u -p %s' % str(self.__port_iperf))
+            self.__s.exec_command('iperf -s -i 1 -u -p %s'
+                                  % str(self.__port_iperf))
             time.sleep(3)
-            os.system('iperf -c %s -i 1 -u -b 100000000 -p %s |tee %s' % (self.__hostname, str(self.__port_iperf), self.__filename))
+            os.system('iperf -c %s -i 1 -u -b 100000000 -p %s |tee %s'
+                      % (self.__hostname,
+                         str(self.__port_iperf),
+                         self.__filename))
             time.sleep(2)
             print ("****************************************************")
-            stdin, stdout, stderr = self.__s.exec_command('killall iperf')
+            self.__s.exec_command('killall iperf')
             self.__s.close()
-            # stdin, stdout, stderr = self.s.exec_command('ps ax|grep iperf')
             return 0
 
     def UDP_Upload(self):
@@ -76,16 +81,17 @@ class iperf(SSH_connection):
         elif(self.__s == -2):
             return -2
         else:
-            os.system('iperf -s -i 1 -u -p %s | tee %s &' % (str(self.__port_iperf), self.__filename))
+            os.system('iperf -s -i 1 -u -p %s | tee %s &'
+                      % (str(self.__port_iperf), self.__filename))
             time.sleep(2)
-            stdin, stdout, stderr = self.__s.exec_command(
-                'iperf -c %s -i 1 -u -b 100000000 -p %s' % (self.__master_IP, str(self.__port_iperf)))
+            stdout = self.__s.exec_command(
+                'iperf -c %s -i 1 -u -b 100000000 -p %s'
+                % (self.__master_IP, str(self.__port_iperf)))
             print(stdout.read())
             time.sleep(2)
             print ("****************************************************")
             os.system('killall iperf')
             self.__s.close()
-            # stdin, stdout, stderr = self.s.exec_command('ps ax|grep iperf')
             return 0
 
     def list_parameters(self):
